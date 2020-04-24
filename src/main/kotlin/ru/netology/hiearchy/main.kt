2175abd6af3f98bc4ae274bca296085aa0b54f92
Dmitriy25 <ohapkin_da@mail.ru>
2020-04-24 09:27:22 +0300

    Версия на проверку

--- src/main/kotlin/ru/netology/hiearchy/main.kt
+++ src/main/kotlin/ru/netology/hiearchy/main.kt
@@ -1,27 +1,11 @@
 package ru.netology.hiearchy
 
 import ru.netology.hiearchy.view.TextView
-import ru.netology.hiearchy.view.View
 import ru.netology.hiearchy.view.ViewGroup
 import ru.netology.hiearchy.widget.Button
 
 fun main() {
-    //Тестирование работоспособности
-    val view = View()
-    view.click()
 
-    val textView = TextView("Some text")
-    textView.click()
-    textView.text = "Changed text"
-    println(textView.text)
-
-    val button = Button("Click me!")
-    button.click()
-    button.text = "Don't click me"
-    println(button.text)
-
-    //TODO добавить тесты
-    // Основной код
     val main = ViewGroup()
     val title = TextView("Main screen")
     main.addView(title) //Кастовать не надо
